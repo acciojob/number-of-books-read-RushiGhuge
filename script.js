dfdc63611bf244aeda68f33cb7e3deec241a9a33
Rushikesh Ghuge <100513228+RushiGhuge@users.noreply.github.com>
2023-08-09 07:30:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -18,6 +18,14 @@ const library = [
 
 const numberOfBooksRead = () => {
   // write your code here
+
+	let count = 0;
+	library.forEach(arr=>{
+		if(arr.readingStatus){
+			count++;
+		}
+	})
+	return count;
 };
 
 // Do not change the code below
